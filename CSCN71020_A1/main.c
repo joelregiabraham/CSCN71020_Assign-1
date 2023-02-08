070e9fc41bf02c5c6af4b3cedc9567af7ce13fcc
joelregiabraham <jregiabraham5574@conestogac.on.ca>
2023-02-07 19:32:05 -0500

    Substraction function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void Sub();
 
 
 void main() {
@@ -20,23 +21,43 @@ void main() {
 	{
 	case 1:
 		add();
+	
+	case 2:
+		Sub();
+
 	}
-
+}
+void printWelcomeMenu() 
+{
+			printf(" **********************\n");
+			printf("**   Welcome to the   **\n");
+			printf("**   BCS Calculator   **\n");
+			printf(" **********************\n");
 }
 
-void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
+void printOptions() 
+{
+			printf("1. Add\n");
+			printf("2. Subtract\n");
 }
 
-void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
-}
+void add() 
+{
 
-void add() {
+			double num1, num2, num3, result;
+			printf("Enter the first value:");
+			scanf_s("%lf", &num1);
+			printf("Enter the second value:");
+			scanf_s("%lf", &num2);
+			printf("Enter the third value:");
+			scanf_s("%lf", &num3);
+			result = num1 + num2 + num3;
+			printf("%lf + %lf = %lf\n", num1, num2, result);
+ }
+
+void Sub()
+{
+
 	double num1, num2, num3, result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
@@ -44,6 +65,9 @@ void add() {
 	scanf_s("%lf", &num2);
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
-	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
+
 }
+	
+
